c3b0866cfb5d7d5026a6c242764e477598138c73
Shelo <siankhual96@gmail.com>
2019-08-06 15:37:54 +0800

    deploy again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
     def welcome_email(user)
         @user = user
-        @login_url = 'http://example.com/login'
+        @login_url = 'www.biblichor.site'
         mail(to: @user.email, subject: 'Welcome to Biblichor')
     end
 end
