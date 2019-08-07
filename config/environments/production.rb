b4a626589c22b0a2bc2024fc21115bd3d47afa22
Shelo <siankhual96@gmail.com>
2019-08-07 14:19:29 +0800

    what

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'www.biblichor.site' }
+
 
   # Gmail configuration
   config.action_mailer.raise_delivery_errors = true
